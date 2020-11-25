86d71f3a21a51020b8332468198ee5cdad95d43a
Hermogenes00 <neto.programer@gmail.com>
2020-11-25 11:57:46 -0300

    Codificando cálculo de largura * altura, product->detail

--- public/js/product.js
+++ public/js/product.js
@@ -1,23 +1,65 @@
 
-let qtd = document.getElementsByName('qtd')
+
 let vlrel = document.getElementById('vlrel');
 let idProduto = document.getElementById('idProduto')
 let vlrTotalel = document.getElementById('vlrTotal');
 
-let propriedadeDivisao = parseFloat(qtd[0].dataset.propriedadedivisao);
+let formaMedicao = document.getElementById('formaMedicao')
 
-//console.log(qtd[0].dataset.propriedadedivisao);
-//Formatando valor do item
+function vlrTotalMetroQuadrado(largura, altura) {
+    let result = 0
+    if (largura != undefined && !isNaN(largura)) {
+        if (altura != undefined && !isNaN(altura)) {
+            result = largura * altura
+            vlrTotalel.innerHTML = parseFloat(result).toLocaleString('pt-br')
+            return result
 
-qtd[0].addEventListener('change', (event) => {
+        } else {
+            vlrTotalel.innerHTML = parseFloat('0').toLocaleString('pt-br')
+            return 0;
+        }
+    } else {
+        vlrTotalel.innerHTML = parseFloat('0').toLocaleString('pt-br')
+        return 0;
+    }
+}
 
-    let qtd = parseInt(event.target.value) / propriedadeDivisao;
+if (formaMedicao.und == 'und') {
 
-    vlrTotalel.innerText = (qtd * parseFloat(vlrel.innerText)).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
-    vlrel.toLocaleString('pt-br')
+    let qtd = document.getElementsByName('qtd')
+
+    let propriedadeDivisao = parseFloat(qtd[0].dataset.propriedadedivisao);
+    //console.log(qtd[0].dataset.propriedadedivisao);
+    //Formatando valor do item
+
+    qtd[0].addEventListener('change', (event) => {
+
+        let qtd = parseInt(event.target.value) / propriedadeDivisao;
+
+        vlrTotalel.innerText = (qtd * parseFloat(vlrel.innerText)).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+        vlrel.toLocaleString('pt-br')
 
 
-})
+    })
+} else {
+
+    let altura = document.getElementById('altura')
+    let largura = document.getElementById('largura')
+
+    largura.addEventListener('keyup', (event) => {
+        let result = formatReal(event.target.value)
+        event.target.value = result;
+        vlrTotalMetroQuadrado(largura.value, altura.value)
+    })
+
+    altura.addEventListener('keyup', (event) => {
+        let result = formatReal(event.target.value)
+        event.target.value = result;
+        vlrTotalMetroQuadrado(largura.value, altura.value)
+    })
+}
+
+
 
 
 let divDescricao = document.getElementById('descricao');
